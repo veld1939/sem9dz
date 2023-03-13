136fdceb768062543d5cbd5d4822de38e35f794a
veld <1091939@bk.ru>
2023-03-13 14:46:03 +0300

    dz

---
+++ dz1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void RoadTon(int number )
+{
+    
+    if (number ==0)
+     {
+        return;
+     }
+     System.Console.Write($" {number}");
+      RoadTon(number - 1);
+
+} 
+
+ System.Console.Write("Введите число: " );
+ int number = Convert.ToInt32(Console.ReadLine());
+ RoadTon( number );
---
+++ dz2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+System.Console.Write("Введите число: " );
+ int numberM = Convert.ToInt32(Console.ReadLine());
+ System.Console.Write("Введите число: " );
+ int numberN = Convert.ToInt32(Console.ReadLine());
+RoadTonSum( numberM , numberN);
+
+void RoadTonSum(int numberM ,int numberN)
+{
+    Console.Write(SumMiN(numberM - 1, numberN));
+}
+int SumMiN(int numberM,int numberN)
+{
+    int result = numberM;
+    {
+        if(numberM == numberN)
+        return 0;
+        else
+        {
+            numberM++;
+            result= numberM + SumMiN(numberM,numberN);
+            return result;
+        }
+    }
+}
+
+
+
+
+
